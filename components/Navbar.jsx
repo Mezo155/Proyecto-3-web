360c882ed6faff45a46aae8b0a2102f6ae0268e9
Mezo155 <tonicb155@hotmail.com>
2024-06-21 23:55:49 +0200

    2 parte

---
+++ components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
+import { logout } from '../stores/AccessTokenStore';
+
+function Navbar() {
+  const { user } = useContext(AuthContext)
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Pagina Pelis</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/apartments/add">Add apartment</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          {user && <button onClick={logout} className="btn btn-danger">Logout</button>}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
