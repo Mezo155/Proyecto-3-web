1e4e13a54a13dfa8a712c42bfbe3ee78a5914eda
Mezo155 <tonicb155@hotmail.com>
2024-07-03 17:01:40 +0200

    bug comentario

--- src/services/filmsServices.js
+++ src/services/filmsServices.js
@@ -17,14 +17,13 @@ export const getMyLikes = () => {
   return http.get("/likes/me")
 }
 
-export const createComment = (commentData) => {
-  const { filmId, ...rest } = commentData;
-  console.log('Received filmId:', filmId); // Verificar si filmId está definido aquí
+export const createComment = (filmId) => {
+  console.log(filmId)
   if (!filmId) {
-    console.error('filmId is undefined in createComment');
-    return Promise.reject('filmId is undefined');
+    console.error('externalItemId is undefined at likeFilm');
+    return Promise.reject('externalItemId is undefined');
   }
 
-  console.log('filmId in createComment:', filmId);
-  return http.post(`/films/${filmId}/comment`, rest); // Pasa el resto de los datos del comentario en el cuerpo de la solicitud
+  console.log('externalItemId in likeFilm:', filmId);
+  return http.post(`/films/${filmId}/comment`);
 };
